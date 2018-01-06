12500e0a323ba026eafe1d389c3f5a33601f3949
Pande Putu Widya Oktapratama <widya.oktapratama@gmail.com>
2018-01-06 15:03:21 +0800

    Base Templating Done

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="side-navbar">
+  <div class="sidebar-header d-flex align-items-center">
+    <div class="avatar"><img src="{{asset('')}}/img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
+    <div class="title">
+      <h1 class="h4">Mark Stephen</h1>
+      <p>Web Designer</p>
+    </div>
+  </div>
+  @include('layouts.navigation.'.$nav)
+</nav>
---
+++ resources/views/layouts/navigation/bendahara.blade.php
@@ -0,0 +1,21 @@
+<span class="heading">Main</span>
+<ul class="list-unstyled">
+          <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+          <li><a href="tables.html"> <i class="icon-grid"></i>Tables </a></li>
+          <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
+          <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
+          <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-interface-windows"></i>Example dropdown </a>
+            <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+              <li><a href="#">Page</a></li>
+              <li><a href="#">Page</a></li>
+              <li><a href="#">Page</a></li>
+            </ul>
+          </li>
+          <li><a href="login.html"> <i class="icon-interface-windows"></i>Login page </a></li>
+</ul><span class="heading">Extras</span>
+<ul class="list-unstyled">
+  <li> <a href="#"> <i class="icon-flask"></i>Demo </a></li>
+  <li> <a href="#"> <i class="icon-screen"></i>Demo </a></li>
+  <li> <a href="#"> <i class="icon-mail"></i>Demo </a></li>
+  <li> <a href="#"> <i class="icon-picture"></i>Demo </a></li>
+</ul>
