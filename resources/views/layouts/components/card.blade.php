6b3af205dc606098bb32704fcdca4599fdad95e1
Pande Putu Widya Oktapratama <widya.oktapratama@gmail.com>
2018-01-07 12:00:52 +0800

    Creating component

---
+++ resources/views/layouts/components/card.blade.php
@@ -0,0 +1,10 @@
+<div class="{{$class}}">
+    <div class="card">
+        <div class="card-header d-flex align-items-center">
+          <h3 class="h4">{{$title}}</h3>
+        </div>
+        <div class="card-body">
+            {{$slot}}
+        </div>
+    </div>
+</div>
