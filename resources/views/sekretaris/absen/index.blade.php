2351377c4faf92dfdb175b6fed881041b43e18a4
pradipta31 <pradiptadipta31@gmail.com>
2018-01-08 16:36:26 +0800

    rapat

--- resources/views/sekretaris/absen/index.blade.php
+++ resources/views/sekretaris/absen/index.blade.php
@@ -40,6 +40,44 @@
                 </div>
             </div>
         </section>
+
+        <section class="forms">
+            <div class="container-fluid">
+                <div class="row">
+                    @component('layouts.components.card',['class' => 'col-md-12','title' => 'Data Absensi'])
+                        <br><br>
+                        <table class="table" id="table">
+                            <thead>
+                                <tr>
+                                    <th>NIM</th>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th>Handphone</th>
+                                    <th>Jenis Kelamin</th>
+                                    <th>Opsi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach (json_decode(config('master.members')) as $member)
+                                    <tr>
+                                        <td>{{$member->nim}}</td>
+                                        <td>{{$member->name}}</td>
+                                        <td>{{$member->email}}</td>
+                                        <td>{{$member->handphone}}</td>
+                                        <td>{{$member->sex}}</td>
+                                        <td>
+                                            <a href="javascript:void();" onclick="editMember()"><i class="fa fa-edit"></i></a>
+                                            <a href="#"><i class="fa fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn btn-primary pull-right" style="margin-right:35px;margin-top:10px">Simpan</button>
+                    @endcomponent
+                </div>
+            </div>
+        </section>
     @endsection
     @push('css')
         <link rel="stylesheet" href="{{asset("vendor/datatable/style.css")}}">
