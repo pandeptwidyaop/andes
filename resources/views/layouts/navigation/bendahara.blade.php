c249a944896a0baeb5be331f9d2c5ba7ecb552ea
pradipta31 <pradiptadipta31@gmail.com>
2018-01-08 22:59:42 +0800

    create bendahara

--- resources/views/layouts/navigation/bendahara.blade.php
+++ resources/views/layouts/navigation/bendahara.blade.php
@@ -1,29 +1,31 @@
 <span class="heading">Main</span>
 <ul class="list-unstyled">
           <li class="{{$nav_active == 'dashboard' ? 'active' : ''}}">
-            <a href="{{asset('bendahara/dashboard')}}"> <i class="icon-home"></i>Home </a>
+            <a href="{{asset('bendahara/dashboard')}}"> <i class="fa fa-dashboard"></i>Dashboard </a>
           </li>
           <li class="{{$nav_active == 'kas' ? 'active' : ''}}">
-            <a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse">
-              <i class="icon-interface-windows"></i>Kas
+            <a href="#dropdownKas" aria-expanded="false" data-toggle="collapse">
+              <i class="fa fa-group"></i>Kas
             </a>
-            <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+            <ul id="dropdownKas" class="collapse list-unstyled ">
               <li><a href="{{asset('bendahara/kas')}}">Data Kas</a></li>
               <li><a href="{{asset('bendahara/tambah-kas')}}">Tambah Kas</a></li>
             </ul>
           </li>
           <li class="{{$nav_active == 'pemasukan' ? 'active' : ''}}">
-          <a href="#exampledropdownDropdown1" aria-expanded="false" data-toggle="collapse">
-            <i class="icon-interface-windows">
+          <a href="#dropdownPemasukan" aria-expanded="false" data-toggle="collapse">
+            <i class="fa fa-money">
             </i>Pemasukan
           </a>
-            <ul id="exampledropdownDropdown1" class="collapse list-unstyled ">
+            <ul id="dropdownPemasukan" class="collapse list-unstyled ">
               <li><a href="{{asset('bendahara/pemasukan')}}">Data Pemasukan</a></li>
               <li><a href="{{asset('bendahara/tambah-pemasukan')}}">Tambah Pemasukan</a></li>
             </ul>
-          </li class="{{$nav_active == 'pengeluaran' ? 'active' : ''}}">
-          <li><a href="#exampledropdownDropdown2" aria-expanded="false" data-toggle="collapse"> <i class="icon-interface-windows"></i>Pengeluaran </a>
-            <ul id="exampledropdownDropdown2" class="collapse list-unstyled ">
+          </li>
+          <li class="{{$nav_active == 'pengeluaran' ? 'active' : ''}}">
+            <a href="#dropdownPengeluaran" aria-expanded="false" data-toggle="collapse">
+              <i class="fa fa-money"></i>Pengeluaran </a>
+            <ul id="dropdownPengeluaran" class="collapse list-unstyled ">
               <li><a href="{{asset('bendahara/pengeluaran')}}">Data Pengeluaran</a></li>
               <li><a href="{{asset('bendahara/tambah-pengeluaran')}}">Tambah Pengeluaran</a></li>
             </ul>
